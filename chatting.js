cc38cba0f516b5d07fa837b507138a58f9fb8bc2
eggbee <ty39563956@gmail.com>
2019-05-29 15:47:00 +0900

    init

---
+++ chatting.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const socket=require('socket.io')
+const http =require('http')
+const app=express()
+const server=http.createServer(app)
+const io =socket(server)
+const fs=require('fs')
+
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
+app.get('/', function(request, response) {
+    fs.readFile('./static/index.html', function(err, data) {
+      if(err) {
+        response.send('에러')
+      } else {
+        response.writeHead(200, {'Content-Type':'text/html'})
+        response.write(data)
+        response.end()
+      }
+    })
+  })
+  
+  io.sockets.on('connection', function(socket) {
+    console.log('유저 접속 됨')
+  
+    socket.on('send', function(data) {
+      console.log('전달된 메시지:', data.msg)
+    })
+  
+    socket.on('disconnect', function() {
+      console.log('접속 종료')
+    })
+  })
+  
+  /* 서버를 8080 포트로 listen */
+  server.listen(8080, function() {
+    console.log('서버 실행 중..')
+  })
